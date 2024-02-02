c61e5e301aaa3f7be5a50a7905c02c82b45360af
kfliemann <kevin.fliemann@gmx.de>
2024-02-02 14:23:52 +0100

    completed slide functionality

--- js/functionality.js
+++ js/functionality.js
@@ -29,26 +29,23 @@ function checkScroll(event) {
 /**
  * @param {*} direction 
  * 0 = down | 1 = up
- * creating new child divs inside of world div will result in new pagestates
+ * creating new child divs inside of slideContainer div will result in new pagestates
  * else if statement in each case is causing infinite scrolling in each direction
  */
 function switchPageState(direction) {
-    world = document.getElementById("world");
-    pageElements = world.children;
+    slideContainer = document.getElementById("slideContainer");
+    pageElements = slideContainer.children;
 
     switch (direction) {
         case 0:
             if (pageState < pageElements.length) {
                 amount = pageState * -100;
-                world.style.top = amount + "vh";
+                slideContainer.style.top = amount + "vh";
                 pageElements[pageState].style.position = "relative"
                 pageState++;
             } else if (pageState == pageElements.length) {
                 pageState = 1;
-                for (let i = 1; i < pageElements.length; i++) {
-                    pageElements[i].style.position = "absolute";
-                }
-                world.style.top = "0vh";
+                slideContainer.style.top = "0vh";
                 pageElements[pageState - 1].style.position = "relative"
             }
             break;
@@ -56,34 +53,25 @@ function switchPageState(direction) {
             if (pageState > 1) {
                 pageState--;
                 amount = (pageState - 1) * -100;
-                world.style.top = amount + "vh";
-                pageElements[pageState].style.position = "absolute"
-
+                slideContainer.style.top = amount + "vh";
             } else if (pageState == 1) {
                 pageState = pageElements.length;
                 for (let i = 0; i < pageState; i++) {
                     pageElements[i].style.position = "relative";
                 }
                 amount = (pageState - 1) * -100;
-                world.style.top = amount + "vh";
+                slideContainer.style.top = amount + "vh";
             }
             break;
         default:
-            console.log("easteregg");   
+            console.log("easteregg");
             break;
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () { 
-
-    document.getElementById("world").addEventListener('transitionstart', function () {
-        //TODO
-    });
-
-    document.getElementById("world").addEventListener('transitionend', function () {
-        //TODO
-    });
-
+document.addEventListener('DOMContentLoaded', function () {
+    slideContainer = document.getElementById("slideContainer");
+    slideContainer.style.height = (slideContainer.children.length * 100) + "vh";
 });
 
 

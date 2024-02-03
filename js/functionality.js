2a582449a8a4e6fff29d05686eb0d3c7549303da
kfliemann <kevin.fliemann@gmx.de>
2024-02-03 14:04:32 +0100

    smooth infinite scrolling + code beautify

--- js/functionality.js
+++ js/functionality.js
@@ -2,7 +2,7 @@
  * this defines, which portfolio page will be displayed
  * body can have n elements, because body has more content than head / footer
  * footer is the n+1th element
- * 1 = head | n >= 2 = body | n+1 = footer 
+ * 0 = prepend footer | 1 = head | n >= 2 = body | n+1 = footer |n+2 = append head
  */
 var pageState = 1;
 
@@ -23,58 +23,77 @@ function checkScroll(event) {
     } else if (delta > 0) {
         switchPageState(1);
     }
-
 }
 
 /**
  * @param {*} direction 
  * 0 = down | 1 = up
  * creating new child divs inside of slideContainer div will result in new pagestates
- * else if statement in each case is causing infinite scrolling in each direction
+ * eventListener is used for an instant transition from top/bottom bottom/top for smoother scrolling
  */
 function switchPageState(direction) {
-    slideContainer = document.getElementById("slideContainer");
-    pageElements = slideContainer.children;
+    let slideContainer = document.getElementById("slideContainer");
+    let pageElements = slideContainer.childElementCount;
+
+    if (!slideContainer.classList.contains("slideContainer")) {
+        slideContainer.classList.add('slideContainer');
+    }
 
     switch (direction) {
         case 0:
-            if (pageState < pageElements.length) {
-                amount = pageState * -100;
-                slideContainer.style.top = amount + "vh";
+            if (pageState < pageElements - 1) {
                 pageState++;
-            } else if (pageState == pageElements.length) {
-                pageState = 1;
-                slideContainer.style.top = "0vh";
+                slideContainer.style.top = pageState * -100 + "vh";
+
+                //remove transition effect, move window to initial pagestate
+                slideContainer.addEventListener('transitionend', function () {
+                    if (pageState == (pageElements - 1)) {
+                        slideContainer.classList.remove('slideContainer');
+                        pageState = 1;
+                        slideContainer.style.top = pageState * -100 + "vh";
+                    }
+                });
             }
             break;
         case 1:
-            if (pageState > 1) {
+            if (pageState >= 1) {
                 pageState--;
-                amount = (pageState - 1) * -100;
-                slideContainer.style.top = amount + "vh";
-            } else if (pageState == 1) {
-                pageState = pageElements.length;
-                amount = (pageState - 1) * -100;
-                slideContainer.style.top = amount + "vh";
+                slideContainer.style.top = pageState * -100 + "vh";
+
+                //remove transition effect, move window to initial pagestate
+                slideContainer.addEventListener('transitionend', function () {
+                    if (pageState == 0) {
+                        slideContainer.classList.remove('slideContainer');
+                        pageState = pageElements - 2;
+                        slideContainer.style.top = pageState * -100 + "vh";
+                    }
+                });
             }
-            break;
-        default:
-            console.log("easteregg");
             break;
     }
 }
 
+/**
+ * append first / prepend last item to list for smoother infinite scroll
+ */
 document.addEventListener('DOMContentLoaded', function () {
-    slideContainer = document.getElementById("slideContainer");
-    slideContainer.style.height = (slideContainer.children.length * 100) + "vh";
+    let slideContainer = document.getElementById("slideContainer");
+    let slideElements = document.getElementById("slideContainer").children;
+    let firstElement = slideElements[0].cloneNode(true);
+    let lastElement = slideElements[slideElements.length - 1].cloneNode(true);
+
+    slideContainer.insertBefore(lastElement, slideElements[0]);
+    slideContainer.appendChild(firstElement);
+
+    slideContainer.style.height = (slideContainer.childElementCount * 100) + "vh";
 
     document.addEventListener('keydown', function (event) {
-        if (event.key === 'ArrowUp' || event.key === 'PageUp'|| event.key === 'w') {
-            switchPageState(1)
-        } else if (event.key === 'ArrowDown' || event.key === 'PageDown' || event.key === 's'){
-            switchPageState(0)
+        if (event.key === 'ArrowUp' || event.key === 'PageUp') {
+            switchPageState(1);
+        } else if (event.key === 'ArrowDown' || event.key === 'PageDown') {
+            switchPageState(0);
         }
-      });
+    });
 });
 
 
